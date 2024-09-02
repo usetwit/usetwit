0d4c92517656603eba0e660e10a61cfd6fc2958e
Lee Jeffries <leecjeffries@gmail.com>
2024-09-02 19:42:50 +0100

    Dropdowns - loop through items

--- resources/js/components/Form/AutoComplete.vue
+++ resources/js/components/Form/AutoComplete.vue
@@ -7,6 +7,14 @@ import { onMounted, ref } from 'vue'
 const props = defineProps({
     items: { type: Array, default: [] },
     dropdown: { type: Boolean, default: false },
+    disabled: { type: Boolean, default: false },
+    positionX: { type: String, default: 'left' },
+    positionY: { type: String, default: 'bottom' },
+    minWidth: { type: [Number, Boolean], default: null },
+    maxHeight: { type: Number, default: null },
+    optionLabel: { type: String, required: true },
+    optionGroupLabel: { type: String, default: null },
+    optionGroupItems: { type: String, default: null },
 })
 
 const {
@@ -16,7 +24,7 @@ const {
     dropdownStyle,
     showDropdown,
     toggleDropdown
-} = useDropdownPosition('left', 'bottom', false, 400)
+} = useDropdownPosition(props.positionX, props.positionY, props.minWidth, props.maxHeight)
 
 const model = defineModel()
 
@@ -27,12 +35,16 @@ onMounted(() => {
         inputRef.value = inputTextRef.value.inputElement
     }
 })
+
+const x = (item) => {
+    model.value = item[props.optionLabel]
+}
 </script>
 
 <template>
     <div class="inline-block">
         <InputGroup>
-            <InputText v-model="model" ref="inputTextRef" style="width: 600px"/>
+            <InputText v-model="model" ref="inputTextRef" style="width: 600px" :disabled="props.disabled"/>
             <button v-if="props.dropdown"
                     class="inline-flex bg-gray-200 hover:bg-gray-100 text-gray-700 items-center py-2.5 px-3 align-middle"
                     @click="toggleDropdown"
@@ -46,10 +58,31 @@ onMounted(() => {
         <Teleport to="body">
             <div v-if="showDropdown"
                  ref="dropdownRef"
-                 class="rounded absolute z-40 bg-white shadow border-gray-400 border p-5"
+                 class="rounded absolute z-40 bg-white shadow border-gray-400 border flex flex-col"
                  :style="dropdownStyle"
             >
-                ffffffffffffff
+                <ul v-if="!props.optionGroupLabel && items.length">
+                    <li v-for="item in items"
+                        @click="x(item)"
+                        class="flex cursor-pointer hover:bg-gray-100 text-gray-700 items-center p-2">
+                        <slot name="item" v-bind="item">{{ item[props.optionLabel] }}</slot>
+                    </li>
+                </ul>
+                <ul v-else-if="props.optionGroupLabel && items.length">
+                    <li v-for="item in items">
+                        <slot name="optiongroup" v-bind="item">
+                            <div class="font-bold">{{ item[props.optionGroupLabel] }}</div>
+                        </slot>
+                        <ul v-if="item[props.optionGroupItems] && item[props.optionGroupItems].length">
+                            <li v-for="subitem in item[props.optionGroupItems]">
+                                <slot name="item" v-bind="subitem">{{ subitem[props.optionLabel] }}</slot>
+                            </li>
+                        </ul>
+                    </li>
+                </ul>
+                <div v-else>
+                    No Results
+                </div>
             </div>
         </Teleport>
     </div>
--- resources/js/components/Home.vue
+++ resources/js/components/Home.vue
@@ -2,18 +2,52 @@
 import AutoComplete from "./Form/AutoComplete.vue";
 import { ref } from "vue";
 
-const items = ref([
+// const items = ref([
+//     {
+//         name: 'Lee', id: 1,
+//     },
+//     {
+//         name: 'Sandra', id: 2,
+//     },
+// ])
+
+const groupedCities = ref([
     {
-        name: 'Lee', id: 1,
+        label: 'Germany',
+        code: 'DE',
+        items: [
+            { label: 'Berlin', value: 'Berlin' },
+            { label: 'Frankfurt', value: 'Frankfurt' },
+            { label: 'Hamburg', value: 'Hamburg' },
+            { label: 'Munich', value: 'Munich' }
+        ]
     },
     {
-        name: 'Sandra', id: 2,
+        label: 'USA',
+        code: 'US',
+        items: [
+            { label: 'Chicago', value: 'Chicago' },
+            { label: 'Los Angeles', value: 'Los Angeles' },
+            { label: 'New York', value: 'New York' },
+            { label: 'San Francisco', value: 'San Francisco' }
+        ]
     },
+    {
+        label: 'Japan',
+        code: 'JP',
+        items: [
+            { label: 'Kyoto', value: 'Kyoto' },
+            { label: 'Osaka', value: 'Osaka' },
+            { label: 'Tokyo', value: 'Tokyo' },
+            { label: 'Yokohama', value: 'Yokohama' }
+        ]
+    }
 ])
 
 const value = ref('jhjhjh')
 </script>
 
 <template>
-    <AutoComplete v-model="value" :items="items" dropdown/>
+    <AutoComplete v-model="value" :items="groupedCities" optionLabel="label" optionGroupLabel="label"
+                  optionGroupItems="items" dropdown min-width/>
 </template>
