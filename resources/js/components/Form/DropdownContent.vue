046baaa12dbddf134be9f30253ac7da5d2e19d7e
Lee Jeffries <leecjeffries@gmail.com>
2024-09-02 17:02:24 +0100

    Dropdowns - fixed top and bottom

---
+++ resources/js/components/Form/DropdownContent.vue
@@ -0,0 +1,19 @@
+<script setup>
+
+</script>
+
+<template>
+    <Teleport to="body">
+        <div v-if="showDropdown"
+             ref="dropdownRef"
+             class="rounded absolute z-40 bg-white shadow border-gray-400 border p-5"
+             :style="dropdownStyle"
+        >
+            <slot></slot>
+        </div>
+    </Teleport>
+</template>
+
+<style scoped>
+
+</style>
