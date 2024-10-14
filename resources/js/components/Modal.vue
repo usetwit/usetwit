98954f1b2a73799eb3a634ae9f2cdc9fbac0bb93
Lee Jeffries <leecjeffries@gmail.com>
2024-10-14 06:41:01 +0100

    User profile delete user Modal

--- resources/js/components/Modal.vue
+++ resources/js/components/Modal.vue
@@ -27,7 +27,8 @@ const accept = () => {
         <div @click.self="isVisible = false"
              class="bg-opacity-50 bg-gray-900 z-[950] w-full h-full left-0 top-0 fixed flex items-center justify-center"
         >
-            <div class="rounded-lg bg-white shadow border-gray-200 border flex flex-col overflow-y-auto p-2 md:p-4 max-w-full max-h-full md:max-w-lg md:max-h-[90vh]">
+            <div
+                class="rounded-lg bg-white shadow border-gray-200 border flex flex-col overflow-y-auto p-2 md:p-4 max-w-full max-h-full md:max-w-lg md:max-h-[90vh]">
                 <h2 v-if="title || $slots.title" class="text-lg font-semibold mb-2 md:mb-4">
                     {{ title }}
                     <slot name="title"/>
@@ -36,8 +37,10 @@ const accept = () => {
                     <slot/>
                 </div>
                 <div class="flex justify-end">
-                    <Button :variant="variant" @click="accept" class="mr-2" :icon="icon" :label="label"></Button>
-                    <Button variant="secondary" @click="close" icon="pi pi-times-circle">Close</Button>
+                    <Button variant="secondary" @click="close" class="mr-2" icon="pi pi-times-circle" border>
+                        Cancel
+                    </Button>
+                    <Button :variant="variant" @click="accept" :icon="icon" :label="label" border></Button>
                 </div>
             </div>
         </div>
