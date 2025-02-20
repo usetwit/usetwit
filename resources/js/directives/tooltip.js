aee71a39e0b4d2f209e1e87ffde11403b45ce746
Michael Maynard <leecjeffries@gmail.com>
2025-02-20 03:57:13 +0000

    WIP tooltip

--- resources/js/directives/tooltip.js
+++ resources/js/directives/tooltip.js
@@ -15,23 +15,23 @@ export default {
             let top, left
             switch (position) {
                 case 'top':
-                    top = rect.top - tooltipRect.height - 8
+                    top = rect.top - tooltipRect.height - 4
                     left = rect.left + (rect.width / 2) - (tooltipRect.width / 2)
                     break
                 case 'bottom':
-                    top = rect.bottom + 8
+                    top = rect.bottom + 4
                     left = rect.left + (rect.width / 2) - (tooltipRect.width / 2)
                     break
                 case 'left':
                     top = rect.top + (rect.height / 2) - (tooltipRect.height / 2)
-                    left = rect.left - tooltipRect.width - 8
+                    left = rect.left - tooltipRect.width - 4
                     break
                 case 'right':
                     top = rect.top + (rect.height / 2) - (tooltipRect.height / 2)
-                    left = rect.right + 8
+                    left = rect.right + 4
                     break
                 default:
-                    top = rect.top - tooltipRect.height - 8
+                    top = rect.top - tooltipRect.height - 4
                     left = rect.left + (rect.width / 2) - (tooltipRect.width / 2)
             }
 
