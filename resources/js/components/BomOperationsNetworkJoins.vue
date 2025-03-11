cdca2cbb719937eeccdef7c740fd5ab082b13867
Michael Maynard <leecjeffries@gmail.com>
2025-03-11 17:31:34 +0000

    Bom Network WIP

--- resources/js/components/BomOperationsNetworkJoins.vue
+++ resources/js/components/BomOperationsNetworkJoins.vue
@@ -25,7 +25,7 @@ const s = ref({
 const pos = computed(() => {
     let pos = {
         x: 'touching',
-        y: 'touching',
+        y: 'middle',
     };
 
     if (s.value.left === op.value.left) {
@@ -34,11 +34,17 @@ const pos = computed(() => {
         pos.x = 'right';
     } else if (s.value.right < op.value.left) {
         pos.x = 'left';
+    } else if (s.value.left < op.value.left && s.value.right > op.value.left) {
+        pos.x = 'leftMid';
+    } else if (s.value.left > op.value.left && s.value.left < op.value.right) {
+        pos.x = 'rightMid';
+    } else if (s.value.left === op.value.right) {
+        pos.x = 'rightEdge';
+    } else if (s.value.right === op.value.left) {
+        pos.x = 'leftEdge';
     }
 
-    if (s.value.top === op.value.top) {
-        pos.y = 'middle';
-    } else if (s.value.top > op.value.top) {
+    if (s.value.top > op.value.top) {
         pos.y = 'below';
     } else if (s.value.top < op.value.top) {
         pos.y = 'above';
@@ -106,7 +112,7 @@ watch(() => ({
              :style="{
                 left: (op.right + (s.left - op.right) / 2) + 'px',
                 top: (s.top + width / 2) + 'px',
-                height: ((op.top + width / 2) - (s.top + width / 2) + 2) + 'px',
+                height: ((op.top + width / 2) - (s.top + width / 2) + 3) + 'px',
              }"></div>
         <div class="h-line end-right"
              :style="{
@@ -134,7 +140,7 @@ watch(() => ({
              :style="{
                 left: (op.right + (s.left - op.right) / 2) + 'px',
                 top: (op.top + width / 2) + 'px',
-                height: ((s.top + width / 2) - (op.top + width / 2) + 2) + 'px',
+                height: ((s.top + width / 2) - (op.top + width / 2) + 3) + 'px',
              }"></div>
         <div class="h-line"
              :style="{
@@ -154,13 +160,110 @@ watch(() => ({
              :style="{
                 left: (op.right + (s.left - op.right) / 2) + 'px',
                 top: (s.top + width / 2) + 'px',
-                height: ((op.top + width / 2) - (s.top + width / 2) + 2) + 'px',
+                height: ((op.top + width / 2) - (s.top + width / 2) + 3) + 'px',
              }"></div>
         <div class="h-line"
              :style="{
                 left: (s.right + (op.left - s.right) / 2) + 'px',
                 top: (op.top + width / 2) + 'px',
                 width: ((op.left - s.right) / 2) + 'px',
+             }"></div>
+    </template>
+    <div v-if="pos.x === 'middle' && pos.y === 'below'"
+         class="v-line end-down"
+         :style="{
+               left: (op.left + width / 2) + 'px',
+               top: op.bottom + 'px',
+               height: (s.top - op.bottom) + 'px',
+         }"
+    ></div>
+    <template v-if="pos.x === 'leftMid' && pos.y === 'below'">
+        <div class="v-line"
+             :style="{
+                left: (op.left + width / 2) + 'px',
+                top: op.bottom + 'px',
+                height: ((s.top - op.bottom) / 2) + 'px',
+             }"></div>
+        <div class="h-line"
+             :style="{
+                left: (s.left + width / 2) + 'px',
+                top: (op.bottom + (s.top - op.bottom) / 2) + 'px',
+                width: (op.left - s.left + 3) + 'px',
+             }"></div>
+        <div class="v-line end-down"
+             :style="{
+                left: (s.left + width / 2) + 'px',
+                top: (op.bottom + (s.top - op.bottom) / 2) + 'px',
+                height: ((s.top - op.bottom) / 2) + 'px',
+             }"></div>
+    </template>
+    <template v-if="pos.x === 'rightMid' && pos.y === 'below'">
+        <div>leftMid</div>
+        <div class="v-line"
+             :style="{
+                left: (op.left + width / 2) + 'px',
+                top: op.bottom + 'px',
+                height: ((s.top - op.bottom) / 2) + 'px',
+             }"></div>
+        <div class="h-line"
+             :style="{
+                left: (op.left + width / 2) + 'px',
+                top: (op.bottom + (s.top - op.bottom) / 2) + 'px',
+                width: (s.right - op.right + 3) + 'px',
+             }"></div>
+        <div class="v-line end-down"
+             :style="{
+                left: (s.left + width / 2) + 'px',
+                top: (op.bottom + (s.top - op.bottom) / 2) + 'px',
+                height: ((s.top - op.bottom) / 2) + 'px',
+             }"></div>
+    </template>
+    <div v-if="pos.x === 'middle' && pos.y === 'above'"
+         class="v-line end-up"
+         :style="{
+               left: (op.left + width / 2) + 'px',
+               top: s.bottom + 'px',
+               height: (op.top - s.bottom) + 'px',
+         }"
+    ></div>
+    <template v-if="pos.x === 'leftMid' && pos.y === 'above'">
+        <div class="v-line end-up"
+             :style="{
+                left: (s.left + width / 2) + 'px',
+                top: s.bottom + 'px',
+                height: ((op.top - s.bottom) / 2) + 'px',
+             }"></div>
+        <div class="h-line"
+             :style="{
+                left: (s.left + width / 2) + 'px',
+                top: (op.bottom + (s.top - op.bottom) / 2) + 'px',
+                width: (op.left - s.left + 3) + 'px',
+             }"></div>
+        <div class="v-line"
+             :style="{
+                left: (op.right - width / 2) + 'px',
+                top: (s.bottom + (op.top - s.bottom) / 2) + 'px',
+                height: ((op.top - s.bottom) / 2) + 'px',
+             }"></div>
+    </template>
+    <template v-if="pos.x === 'rightMid' && pos.y === 'above'">
+        <div class="v-line end-up"
+             :style="{
+                left: (s.left + width / 2) + 'px',
+                top: s.bottom + 'px',
+                height: ((op.top - s.bottom) / 2) + 'px',
+             }"></div>
+        <div class="h-line"
+             :style="{
+                left: (op.left + width / 2) + 'px',
+                top: (s.bottom + (op.top - s.bottom) / 2) + 'px',
+                width: (s.left - op.left + 3) + 'px',
+             }"></div>
+        <div class="v-line"
+             :style="{
+                left: (op.right - width / 2) + 'px',
+                top: (s.bottom + (op.top - s.bottom) / 2) + 'px',
+                height: ((op.top - s.bottom) / 2) + 'px',
              }"></div>
     </template>
 </template>
@@ -192,11 +295,33 @@ watch(() => ({
 .end-left::after {
     content: "";
     position: absolute;
-    left: 5px; /* Position on the left side */
+    left: 5px;
     top: 50%;
     transform: translateX(-100%) translateY(-50%);
     border-style: solid;
-    border-width: 5px 5px 5px 0; /* Use border-right for the arrow */
+    border-width: 5px 5px 5px 0;
     border-color: transparent black transparent transparent;
+}
+
+.end-up::after {
+    content: "";
+    position: absolute;
+    top: 5px;
+    left: 50%;
+    transform: translateX(-50%) translateY(-100%);
+    border-style: solid;
+    border-width: 0 5px 5px 5px;
+    border-color: transparent transparent black transparent;
+}
+
+.end-down::after {
+    content: "";
+    position: absolute;
+    bottom: 5px;
+    left: 50%;
+    transform: translateX(-50%) translateY(100%);
+    border-style: solid;
+    border-width: 5px 5px 0 5px;
+    border-color: black transparent transparent transparent;
 }
 </style>
